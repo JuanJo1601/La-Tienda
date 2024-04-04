2e725878b78eedc1e3afc93338901e871cdbee15
JuanJo1601 <161753224+JuanJo1601@users.noreply.github.com>
2024-04-04 12:45:38 -0500

    Update TIENDA.py

--- TIENDA.py
+++ TIENDA.py
@@ -15,6 +15,12 @@ class Tienda:
    # Metodos
    ----------------------------------------"""
    
+   def __init__(self):
+      self.__producto1 = Producto("Papeleria", "Lapiz", 550.0, 30, 9)
+      self.__producto2 = Producto("Papeleria", "Borrador", 300.0, 15, 5)
+      self.__producto3 = Producto("Supermercado", "Cafe", 5600.0, 20, 10)
+      self.__producto4 = Producto("Drogueria", "Desinfectante", 3200.0, 12, 11)
+      self.dineroEnCaja = 0
    def getProducto1(self):
       return self.__producto1
 
