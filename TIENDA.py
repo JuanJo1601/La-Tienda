04718df47b9a5b11c897f3b7428ed5e262cbae08
JuanJo1601 <161753224+JuanJo1601@users.noreply.github.com>
2024-04-07 23:05:42 -0500

    Update TIENDA.py

--- TIENDA.py
+++ TIENDA.py
@@ -21,7 +21,35 @@ class Tienda:
       self.__producto3 = Producto("Supermercado", "Cafe", 5600.0, 20, 10)
       self.__producto4 = Producto("Drogueria", "Desinfectante", 3200.0, 12, 11)
       self.dineroEnCaja = 0
+
    def getProducto1(self):
       return self.__producto1
+   
+   def darPrecioProducto(self, numeroProducto):
+        if numeroProducto == 1:
+            return self.__producto1.getValorUnitario()
+        elif numeroProducto == 2:
+            return self.__producto2.getValorUnitario()
+        elif numeroProducto == 3:
+            return self.__producto3.getValorUnitario()
+        elif numeroProducto == 4:
+            return self.__producto4.getValorUnitario()
+        
+        else:
+            return None
+        
+   def darPrecioProducto(self, nombreProducto):
+        if nombreProducto == self.__producto1.getNombre():
+            return self.__producto1.getValorUnitario()
+        elif nombreProducto == self.__producto2.getNombre():
+            return self.__producto2.getValorUnitario()
+        elif nombreProducto == self.__producto3.getNombre():
+            return self.__producto3.getValorUnitario()
+        elif nombreProducto == self.__producto4.getNombre():
+            return self.__producto4.getValorUnitario()
+        
+        else:
+            return None
 
+   
     
